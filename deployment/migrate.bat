f551c9ca115a4d202e0a6876d05a66913be661cb
Graham <admin@grahammcculloch.co.uk>
2013-09-13 14:30:27 +0100

    Numerous bug fixes,
    Put staging site in maintenance mode - will need complete db recreate
    (using South)

---
+++ deployment/migrate.bat
@@ -0,0 +1,11 @@
+echo Migrating models
+manage.py migrate core
+manage.py migrate venues
+manage.py migrate competitions
+manage.py migrate teams
+manage.py migrate opposition
+manage.py migrate members
+manage.py migrate matches
+manage.py migrate awards
+manage.py migrate training
+manage.py migrate feedback
