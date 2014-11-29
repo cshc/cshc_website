119bdec648a1635de945f9ef3f798605c5d468c0
Graham <admin@grahammcculloch.co.uk>
2014-11-29 16:13:38 +1100

    Tidied up fabric deployment script

--- deployment/fabfile.py
+++ deployment/fabfile.py
@@ -1,21 +1,23 @@
-##############################################################################
-# PRODUCTION SITE DEPLOYMENT - cambridgesouthhockeyclub.co.uk
-#
-# USE WITH CAUTION!!!
-# ===================
-#
-# These scripts handle the deployment and upgrade of the CSHC site.
-# Example usage: 'fab update_release:v1.2.0'
-# (run from the deployment directory on the command line)
-#
-# NOTE: To rollback a release, you can try this:
-# fab rollback
-#
-# but be aware this is untested!
+"""
+PRODUCTION SITE DEPLOYMENT - cambridgesouthhockeyclub.co.uk
 
-from fabric.api import *
+USE WITH CAUTION!!!
+===================
+
+These scripts handle the deployment and upgrade of the CSHC site.
+Example usage: 'fab update_release:v1.2.0'
+(run from the deployment directory on the command line)
+
+NOTE: To rollback a release, you can try this:
+fab rollback
+
+but be aware this is untested!
+"""
+
+from fabric.api import abort, env, run, lcd, cd, task, local, put
 import os
 import re
+
 
 def _get_env_setting(setting):
     """ Get the environment setting or return exception """
@@ -24,57 +26,18 @@ def _get_env_setting(setting):
     except KeyError:
         abort("Set the %s env variable" % setting)
 
+
 env.hosts = ['sphinx.mythic-beasts.com']
 env.user = _get_env_setting('MYTHIC_BEASTS_SSH_USER')
 env.password = _get_env_setting('MYTHIC_BEASTS_SSH_PASSWORD')
 env.remote_root = '~/new_site/'
 
 DB_BACKUP_FILENAME = 'db_backup.sql'
-
-@task
-def staging_release():
-    local('heroku login')
-    local('git push heroku master')
-    local('heroku restart')
-    # local("heroku run --app cshcsite 'python cshcsite/manage.py syncdb'")
-    # local("heroku run --app cshcsite 'chmod 744 deployment/migrate.sh'")
-    # # Make sure we've got linux line endings
-    # local("heroku run --app cshcsite 'fromdos deployment/migrate.sh'")
-    # local("heroku run --app cshcsite 'deployment/migrate.sh'")
-    # local("heroku run --app cshcsite 'python cshcsite/manage.py create_cshc_superuser'")
-    # local("heroku run --app cshcsite 'python cshcsite/manage.py data_migration'")
-    # local("heroku run --app cshcsite 'python cshcsite/manage.py create_users'")
-    # local("heroku run --app cshcsite 'python cshcsite/manage.py import_data --all'")
-    # local("heroku run --app cshcsite 'python cshcsite/manage.py import_league_tables'")
-
-@task
-def initial_setup(version):
-    check_not_debug()
-    tag_release(version)
-    archive_repo()
-    upload_release()
-    go_live()
-    install_dependencies()
-    syncdb()
-    migrate_db()
-    create_superuser()
-    collectstatic()
-    import_data()
-    set_maintenance_mode(False)
-
-@task
-def file_update(version):
-    check_not_debug()
-    tag_release(version)
-    archive_repo()
-    upload_release()
-    go_live()
-    set_maintenance_mode(True)
-    collectstatic()
-    set_maintenance_mode(False)
+RELEASE_FILENAME = 'repo_release'
 
 @task
 def update_release(version):
+    """ Update the release on the production site."""
     check_not_debug()
     tag_release(version)
     archive_repo()
@@ -84,14 +47,14 @@ def update_release(version):
     set_maintenance_mode(True)
     install_dependencies()
     syncdb()
-    migrate_db()
     collectstatic()
     set_maintenance_mode(False)
 
 @task
 def check_not_debug():
-    with open('../cshcsite/cshcsite/settings/base.py') as f:
-        base_settings = f.read()
+    """ Check that we're not deploying with DEBUG=True set!"""
+    with open('../cshcsite/cshcsite/settings/base.py') as settings_file:
+        base_settings = settings_file.read()
         if "DEBUG = True" in base_settings:
             abort("You must set 'DEBUG = False' in cshcsite/settings/base.py before deploying")
 
@@ -106,35 +69,27 @@ def backup_db():
 
 @task
 def install_dependencies():
+    """ pip install website dependencies."""
     with cd(env.remote_root):
         run('pip install --user -r repo/requirements/production.txt')
 
 @task
 def syncdb():
+    """ Calls syncdb - with the --migrate flag"""
     with cd(env.remote_root + "repo/cshcsite"):
-        run('python manage.py syncdb')
-
-@task
-def create_superuser():
-    with cd(env.remote_root + "repo/cshcsite"):
-        run('python manage.py create_cshc_superuser')
+        run('python manage.py syncdb --migrate')
 
 @task
 def collectstatic():
+    """ Collects the static files (on Amazon S3)"""
     with cd(env.remote_root + "repo/cshcsite"):
         run('python manage.py collectstatic --noinput')
 
 @task
-def import_data():
-    with cd(env.remote_root + "repo/cshcsite"):
-        run('python manage.py data_migration')
-        run('python manage.py create_users')
-        run('python manage.py import_data --all')
-        run('python manage.py import_league_tables')
-
-
-@task
 def tag_release(version):
+    """ Write the specified version string to the version.txt file
+        and commit it to GIT.
+    """
     with lcd(".."):
         local("echo {0} > version.txt".format(version))
         local("git add version.txt")
@@ -143,42 +98,35 @@ def tag_release(version):
 
 @task
 def archive_repo():
+    """ Zip up the local repository"""
     with lcd(".."):
         local("git archive --output repo_release.tar master")
         local("bzip2 repo_release.tar")
 
 @task
 def upload_release():
-    put(local_path='../{}'.format('repo_release.tar.bz2'), remote_path=env.remote_root)
+    """ Upload the zipped repository"""
+    put(local_path='../{0}'.format(RELEASE_FILENAME + '.tar.bz2'), remote_path=env.remote_root)
     with cd(env.remote_root):
-        run('mkdir repo_release')
-        run('tar -jxf repo_release.tar.bz2 -C ./repo_release')
-        run('rm repo_release.tar.bz2')
+        run('mkdir {0}'.format(RELEASE_FILENAME))
+        run('tar -jxf {0}.tar.bz2 -C ./{0}'.format(RELEASE_FILENAME))
+        run('rm {0}.tar.bz2'.format(RELEASE_FILENAME))
     with lcd(".."):
-        local("rm repo_release.tar.bz2")
+        local("rm {0}.tar.bz2".format(RELEASE_FILENAME))
 
 @task
-def temp():
+def set_maintenance_mode(turn_on):
+    """ Sets the MAINTENANCE_MODE flag to True or False in the settings file."""
     with cd(env.remote_root):
-        run('mkdir repo_release')
-        run('tar -jxf repo_release.tar.bz2 -C ./repo_release')
-
-@task
-def set_maintenance_mode(on):
-    with cd(env.remote_root):
-        _add_or_replace_line('repo/cshcsite/cshcsite/settings/production.py', 'MAINTENANCE_MODE = ', str(on))
+        add_or_replace_line('repo/cshcsite/cshcsite/settings/production.py', 'MAINTENANCE_MODE = ',
+                            str(turn_on))
         run('touch ~/www.cambridgesouthhockeyclub.co.uk_html/django.fcgi')
 
 @task
-def migrate_db():
-    with cd(env.remote_root + 'repo/cshcsite/'):
-        run('chmod 744 ../deployment/migrate.sh')
-        # Make sure we've got linux line endings
-        run('fromdos ../deployment/migrate.sh')
-        run('../deployment/migrate.sh')
-
-@task
 def go_live():
+    """ Swap in the new release directory for the previous release directory
+        and touch the FastCGI script to force the webserver to refresh.
+    """
     with cd(env.remote_root):
         run('rm -fr repo_old')
         run('mv repo repo_old')
@@ -203,12 +151,49 @@ def rollback():
         # restores the database using the mysqldump from the previous release
         run('./repo/deployment/restore_db.sh repo/{}'.format(DB_BACKUP_FILENAME))
 
-def _add_or_replace_line(file, pattern, value):
+
+# Uncomment the @task line to enable this again (disabled for safety)
+#@task
+def initial_setup(version):
+    """ Initial setup of the production environment."""
+    check_not_debug()
+    tag_release(version)
+    archive_repo()
+    upload_release()
+    go_live()
+    install_dependencies()
+    syncdb()
+    create_superuser()
+    collectstatic()
+    import_data()
+    set_maintenance_mode(False)
+
+# Uncomment the @task decorator to re-enable this task
+#@task
+def create_superuser():
+    """ Creates the CSHC superuser (using a bespoke Django management command)"""
+    with cd(env.remote_root + "repo/cshcsite"):
+        run('python manage.py create_cshc_superuser')
+
+
+# Uncomment the @task decorator to re-enable this task
+#@task
+def import_data():
+    """ Import data from the CSV exports from the old Access database."""
+    with cd(env.remote_root + "repo/cshcsite"):
+        run('python manage.py data_migration')
+        run('python manage.py create_users')
+        run('python manage.py import_data --all')
+        run('python manage.py import_league_tables')
+
+
+def add_or_replace_line(filename, pattern, value):
+    """ Adds or replaces the given pattern in 'filename' with the supplied value"""
     # Prevent expansion of env vars....
     pattern_find = pattern.replace('$', '\\'+re.escape("$"))
-    found = run('grep "^{term}" {target}||echo missing'.format(term=pattern_find, target=file))
-    if found[0:7] != "missing" :
-        run("sed -i 's/^{term}.*$/{term}".format(term=pattern) + value + "/' " + file)
+    found = run('grep "^{term}" {target}||echo missing'.format(term=pattern_find, target=filename))
+    if found[0:7] != "missing":
+        run("sed -i 's/^{term}.*$/{term}".format(term=pattern) + value + "/' " + filename)
     else:
         line = re.escape(pattern) + value
-        run("echo " + line + ">>" + file)
+        run("echo " + line + ">>" + filename)
