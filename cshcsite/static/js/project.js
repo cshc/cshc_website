0961f8659949c6a6a6923707d73b15a7a318a619
Graham <admin@grahammcculloch.co.uk>
2015-01-05 20:07:12 +1100

    Minor fix to jump-to list scrolling

--- cshcsite/static/js/project.js
+++ cshcsite/static/js/project.js
@@ -36,10 +36,10 @@ function applink(fail){
 }
 
 function scrollToTop(element, parent){
-    var topOffset = 69; // Height of .navbar-fixed-top (when sticky)
-    //if ($('.navbar-fixed-top').length) {
-    //    topOffset += $('#top-nav').height();
-    //}
+    var topOffset = 0;
+    if ($('.navbar-fixed-top').length) {
+        topOffset += 69 //$('#top-nav').height();
+    }
     $(parent).animate({ scrollTop: $(element).offset().top - $(parent).offset().top - topOffset }, { duration: 'slow', easing: 'swing'});
 }
 
