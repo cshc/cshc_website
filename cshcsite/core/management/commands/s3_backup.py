e92444550aedd630266ff4c8da66fd2f4b0cef2d
Graham <admin@grahammcculloch.co.uk>
2015-01-14 18:42:16 +1100

    Fixed minor bug with s3_backup command
    And added print statements

--- cshcsite/core/management/commands/s3_backup.py
+++ cshcsite/core/management/commands/s3_backup.py
@@ -22,7 +22,7 @@ from django.core.mail import send_mail
 PROD_BUCKET_NAME = 'cshc'
 STAGING_BUCKET_NAME = 'cshc-c9'
 BACKUPS_KEY_PREFIX = 'django-dbbackups'
-BACKUP_FILENAME = '~/prod_dump.mysql'
+BACKUP_FILENAME = '/home/ubuntu/prod_dump.mysql'
 GZ_BACKUP_FILENAME = BACKUP_FILENAME + '.gz'
 
 class Command(BaseCommand):
@@ -34,18 +34,18 @@ class Command(BaseCommand):
             prod_bucket = conn.get_bucket(PROD_BUCKET_NAME)
             c9_bucket = conn.get_bucket(STAGING_BUCKET_NAME)
 
-            # 1. Download and unzip the latest MySQL database backup from its S3 storage location
+            print "1. Downloading latest MySQL database backup from S3"
             backups = list(prod_bucket.list(prefix=BACKUPS_KEY_PREFIX))
             latest_backup = sorted(backups, key=lambda b: b.last_modified, reverse=True)[0]
 
             latest_backup.get_contents_to_filename(GZ_BACKUP_FILENAME)
 
+            print "2. Unzipping MySQL database backup to {}".format(BACKUP_FILENAME)
             with gzip.open(GZ_BACKUP_FILENAME, 'rb') as in_file:
                 with open(BACKUP_FILENAME, 'wb') as out_file:
                     out_file.write(in_file.read())
 
-            # 2. Copy all keys from production bucket's media/ directory to the corresponding
-            #    staging bucket's media/ directory
+            print "3. Copy all keys (files) from production bucket's media directory to corresponding staging bucket's media directory"
             for media_key in prod_bucket.list(prefix='media'):
                 c9_bucket.copy_key(media_key.name, PROD_BUCKET_NAME, media_key.name, preserve_acl=True)
 
